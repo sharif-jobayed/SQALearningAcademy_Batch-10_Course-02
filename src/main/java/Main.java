d135e36bc34c5215676857be60e19cac79a5884a
sharif-jobayed <sharif.jobayed@gmail.com>
2024-08-31 02:00:25 +0600

    Done homework

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import identifiers.Identifiers_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Identifiers_01 identifiers_01 = new Identifiers_01();
+
+        System.out.println(identifiers_01.person());
+    }
+}
